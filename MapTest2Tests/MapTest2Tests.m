5cb2cd2427a8aa1dcc8036fc52faec34d4b21d58
Qin Zhibo <satan39557@126.com>
2014-09-20 11:15:59 +0800

    Initial Commit

---
+++ MapTest2Tests/MapTest2Tests.m
@@ -0,0 +1,34 @@
+//
+//  MapTest2Tests.m
+//  MapTest2Tests
+//
+//  Created by 秦智博 on 14-9-20.
+//  Copyright (c) 2014年 lanou3g.com 蓝鸥科技. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MapTest2Tests : XCTestCase
+
+@end
+
+@implementation MapTest2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
